7ff50e09946131bcd5d0401c8abf7dba721450e4
divyanshukumar@ <divyanshusoni651@gmail.com>
2024-10-06 14:42:38 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ dotenv.config({
     path:"../config/.env"
 })
 const databaseConnection = () => {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect("mongodb+srv://divyanshsoni:divyansh@divyansh.6jc07nb.mongodb.net/?retryWrites=true&w=majority&appName=Divyanshz").then(()=>{
         console.log("Connected to mongoDB");
     }).catch((error)=>{
         console.log(error);
